e7f141933b014af73e2a4dd79a4716407d81de1d
user <likhimsiow@yahoo.com>
2017-01-09 11:38:21 +0800

    Done

--- cookie_spec.rb
+++ cookie_spec.rb
@@ -23,15 +23,20 @@ describe Cookie do
   end
 
   describe "#type" do
-    it "returns the type of the cookie"
-
+    it "returns the type of the cookie" do
+      expect(cookie.type).to eq type
+    end
   end
 
   describe "#bake!" do
-    it "requires an integer time argument"
+    it "requires an integer time argument" do
+      #when no argument is given
+      expect { cookie.bake! }.to raise_error(ArgumentError)
+    end
 
-
-    it "returns the cookie object"
+    it "returns the cookie object" do
+      expect(cookie.bake!(10)).to be_kind_of(Cookie)
+    end
 
 
     it "changes the status of the cookie when given enough time" do
@@ -40,32 +45,46 @@ describe Cookie do
   end
 
   describe "#status" do
-    it "returns the cookie's current status"
+    it "returns the cookie's current status" do
+      cookie.bake!(5)
+      expect(cookie.status).to eq :doughy
+    end
 
 
     context "when unbaked" do
-      it "is `:doughy`"
-
+      it "is `:doughy`" do
+      #when no time argyment is given
+        expect(cookie.status).to eq :doughy
+      end
     end
 
     context "when baked for less than 7 minutes" do
-      it "is `:doughy`"
-
+      it "is `:doughy`" do
+        cookie.bake!(5)
+        expect(cookie.status).to eq :doughy
+      end
     end
 
     context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`"
-
+      it "is `:almost_ready`" do
+        cookie.bake!(8)
+        expect(cookie.status).to eq :almost_ready
+      end
     end
 
     context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`"
-
+      it "is `:ready`" do
+        cookie.bake!(11)
+        expect(cookie.status).to eq :ready
+      end
     end
 
     context "when baked for at least 12 minutes" do
-      it "is `:burned`"
-
+      it "is `:burned`" do
+        cookie.bake!(13)
+        expect(cookie.status).to eq :burned
+      end
     end
+
   end
 end
